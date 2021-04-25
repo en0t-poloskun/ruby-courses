4f1c51699dc343e9d5b07ccdc90c252b075cc244
Мария <golovneva.maria@yandex.ru>
2021-04-25 18:14:44 +0300

    methods for CargoWagom class added

--- cargo_wagon.rb
+++ cargo_wagon.rb
@@ -1,7 +1,30 @@
 # frozen_string_literal: true
 
 class CargoWagon < Wagon
-  def initialize
+  attr_reader :occupied_volume
+
+  def initialize(volume)
+    @volume = volume
+    @occupied_volume = 0
     super('грузовой')
+  end
+
+  def fill_with(some_volume)
+    return if some_volume > free_volume
+
+    self.occupied_volume += some_volume
+  end
+
+  def free_volume
+    volume - occupied_volume
+  end
+
+  private
+
+  attr_accessor :volume
+  attr_writer :occupied_volume
+
+  def validate!
+    raise 'Invalid volume' if volume.negative?
   end
 end
