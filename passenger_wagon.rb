056cb493352a6b4637a0c04e4320e8a8e76ad03f
Мария <golovneva.maria@yandex.ru>
2021-04-25 17:46:02 +0300

    methods for PassengerWagon class added

--- passenger_wagon.rb
+++ passenger_wagon.rb
@@ -1,7 +1,30 @@
 # frozen_string_literal: true
 
 class PassengerWagon < Wagon
-  def initialize
+  attr_reader :occupied_seats
+
+  def initialize(seats)
+    @seats = seats
+    @occupied_seats = 0
     super('пассажирский')
+  end
+
+  def take_seat
+    return if occupied_seats == seats
+
+    self.occupied_seats += 1
+  end
+
+  def free_seats
+    seats - occupied_seats
+  end
+
+  private
+
+  attr_accessor :seats
+  attr_writer :occupied_seats
+
+  def validate!
+    raise 'Invalid number of seats' if seats.negative?
   end
 end
